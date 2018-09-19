7f9d63096d191fc9ddae98d6536f3f77daaa15b2
涛袁 <yuantao@taoyuandeMacBook-Pro.local>
2018-09-20 00:33:35 +0800

    fix include

--- GaiaLib/memorypool.h
+++ GaiaLib/memorypool.h
@@ -1,47 +1,49 @@
 #pragma once
+#include <stdio.h>
 namespace Gaia
 {
-	template <class T, int BLOCK_SIZE = 50>
-	class MemoryPool 
+template <class T, int BLOCK_SIZE = 50>
+class MemoryPool
+{
+  public:
+	~MemoryPool() {}
+
+	static void *operator new(size_t size)
 	{
-	public:
-		~MemoryPool(){}
+		if (!allocBuffer)
+			allocBlock();
+		char *pointer = allocBuffer;
+		allocBuffer = *(char **)(pointer);
+		printf("return: %p\n", pointer);
+		return pointer;
+	}
 
-		static void* operator new(size_t size)
+	static void operator delete(void *pointer)
+	{
+		*(char **)(pointer) = allocBuffer;
+		allocBuffer = static_cast<char *>(pointer);
+	}
+
+  private:
+	static void allocBlock()
+	{
+		allocBuffer = new char[sizeof(T) * BLOCK_SIZE];
+		char **pCurrent = (char **)(allocBuffer);
+		char *pNext = allocBuffer;
+		for (int i = 0; i < BLOCK_SIZE - 1; i++)
 		{
-			if (!allocBuffer)
-				allocBlock();
-			char *pointer = allocBuffer;
-			allocBuffer = *(char **)(pointer);
-			printf("return: %p\n", pointer);
-			return pointer;
+			pNext += sizeof(T);
+			*pCurrent = pNext;
+			pCurrent = (char **)pNext;
 		}
+		*pCurrent = 0;
+	}
 
-		static void	operator delete(void* pointer)
-		{
-			*(char**)(pointer) = allocBuffer;
-			allocBuffer = static_cast<char*>(pointer);
-		}
+  private:
+	static char *allocBuffer;
+};
 
-	private:
-		static void	allocBlock()
-		{
-			allocBuffer = new char[sizeof(T) * BLOCK_SIZE];
-			char **pCurrent = (char **)(allocBuffer);
-			char *pNext = allocBuffer;
-			for (int i=0; i<BLOCK_SIZE-1; i++)
-			{
-				pNext += sizeof(T);
-				*pCurrent = pNext;
-				pCurrent = (char **)pNext;
-			}
-			*pCurrent = 0;
-		}
-	private:
-		static char	*allocBuffer;
-	};
+template <class T, int BLOCK_SIZE>
+char *MemoryPool<T, BLOCK_SIZE>::allocBuffer;
 
-	template <class T, int BLOCK_SIZE>
-	char* MemoryPool<T, BLOCK_SIZE>::allocBuffer;
-
-}
+} // namespace Gaia
